871fa10cf0b505e00caceaa807e6baf9f1411d47
Michael Vogt <michael@amutable.com>
2026-02-16 12:27:51 +0100

    wip: add dir backend

--- src/ctl/push.py
+++ src/ctl/push.py
@@ -7,9 +7,12 @@ snapshots to configured remote storage.
 # pylint: disable=duplicate-code,invalid-name,too-few-public-methods
 
 import contextlib
+import errno
 import os
 
 import boto3
+
+from . import util
 
 
 class Push(contextlib.AbstractContextManager):
@@ -97,3 +100,93 @@ class Push(contextlib.AbstractContextManager):
             Bucket="rpmrepo-storage",
             Key=f"data/thread/{snapshot_id}/{snapshot_id}{snapshot_suffix}",
         )
+
+    def push_data_dir(self, output):
+        """Push data to a local directory"""
+
+        assert os.access(os.path.join(self._path_conf, "index.ok"), os.R_OK)
+
+        data_dir = os.path.join(output, "data")
+        os.makedirs(data_dir, exist_ok=True)
+
+        n_total = 0
+        for _, _, entries in os.walk(self._path_data):
+            for entry in entries:
+                n_total += 1
+
+        i_total = 0
+        for level, _, entries in os.walk(self._path_data):
+            for entry in entries:
+                i_total += 1
+                target = os.path.join(data_dir, entry)
+
+                if os.path.exists(target):
+                    print(f"[{i_total}/{n_total}] 'data/{entry}' (exists)")
+                    continue
+
+                print(f"[{i_total}/{n_total}] 'data/{entry}'")
+
+                source = os.path.join(level, entry)
+                os.link(source, target)
+
+    def push_snapshot_dir(self, output, snapshot_id, snapshot_suffix):
+        """Push snapshot to a local directory"""
+
+        assert os.access(os.path.join(self._path_conf, "index.ok"), os.R_OK)
+
+        data_dir = os.path.join(output, "data")
+
+        n_total = 0
+        for _, _, entries in os.walk(self._path_snapshot):
+            for entry in entries:
+                n_total += 1
+
+        i_total = 0
+        for level, _subdirs, entries in os.walk(self._path_snapshot):
+            levelpath = os.path.relpath(level, self._path_snapshot)
+            if levelpath == ".":
+                path = snapshot_id + snapshot_suffix
+            else:
+                path = os.path.join(snapshot_id + snapshot_suffix, levelpath)
+
+            target_dir = os.path.join(output, path)
+            os.makedirs(target_dir, exist_ok=True)
+
+            for entry in entries:
+                i_total += 1
+
+                with open(os.path.join(level, entry), "rb") as filp:
+                    checksum = filp.read().decode()
+
+                target = os.path.join(target_dir, entry)
+                data_file = os.path.join(data_dir, checksum)
+
+                print(f"[{i_total}/{n_total}] '{path}/{entry}' -> data/{checksum}")
+
+                with util.suppress_oserror(errno.ENOENT):
+                    os.unlink(target)
+                os.link(data_file, target)
+
+        thread_dir = os.path.join(output, "thread", snapshot_id)
+        os.makedirs(thread_dir, exist_ok=True)
+        thread_marker = os.path.join(thread_dir, snapshot_id + snapshot_suffix)
+        with open(thread_marker, "a"):
+            pass
+
+    @staticmethod
+    def gc_dir(output):
+        """Remove unreferenced data files from a local directory"""
+
+        data_dir = os.path.join(output, "data")
+        n_deleted = 0
+        n_kept = 0
+
+        for entry in os.listdir(data_dir):
+            path = os.path.join(data_dir, entry)
+            if os.stat(path).st_nlink == 1:
+                os.unlink(path)
+                n_deleted += 1
+            else:
+                n_kept += 1
+
+        print(f"GC: deleted {n_deleted}, kept {n_kept}")
